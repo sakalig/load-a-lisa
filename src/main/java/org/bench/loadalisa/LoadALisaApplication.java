cb795cb4273a196090bbd14227e01b2f919f8938
Gabriel Sakali <gabedevs@gmail.com>
2025-02-13 11:57:19 +0300

    Initial commit

---
+++ src/main/java/org/bench/loadalisa/LoadALisaApplication.java
@@ -0,0 +1,13 @@
+package org.bench.loadalisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadALisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadALisaApplication.class, args);
+	}
+
+}
